e33306cdb1de382e0ce63ff5bc5ba64b9fcdc120
Richard van 't Hof <hello@therichard.space>
2025-09-13 14:26:36 +0200

    Made a Truchet Tiles grid

--- Assignment1/README.md
+++ Assignment1/README.md
@@ -1,8 +1,17 @@
-# Assignment 1
+# Assignment 1: Grid Patterns
+
+## 🚀 How to run
+View the result by opening the `index.html` file in your browser.
+
+## 🏛️ Disclaimer
+Results may be inspired on the code of others. This was done not to copy but to get inspiration for what is possible within the assignment. References to used information can be found in the code's comments.
+
+## 📄 Instructions
 Create a sketch that generates a different grid-based pattern each time it is run by using either the `random()` function or the `noise()` function, e.g., to choose the positions and types of shapes drawn. 
 Marks will be awarded based on technical implementation and visual design. You may use colour and drawing styles, but this is not required.
 
-The examples shown here are based on simple grids, like those you developed in the worksheet exercises, the top one is 20x20 and draws a single diagonal line depending on a conditional test using random(), the bottom one is 15x15 and draws two arcs in opposite corners depending on a conditional text using noise().
+The examples shown here are based on simple grids, like those you developed in the worksheet exercises, the top one is 20x20 and draws a single diagonal line depending on a conditional test using `random()`, the bottom one is 15x15 and draws two arcs in opposite corners depending on a conditional text using `noise()`.
 Note: You are not limited to using square or rectangular grids. If you want to challenge yourself, try creating a triangular or hexangonal grid and using this to draw your pattern.
 
 ![samples](assets/samples.png)
+
--- Assignment1/sketch.js
+++ Assignment1/sketch.js
@@ -1,11 +1,149 @@
-let rows = 20;
-let cols = 20;
+
+// 5. Interactive Truchet Tiles
+// Based on https://www.youtube.com/watch?v=Vpx2OeFc26o
+let rows = 20, cols = 20;
+let tiles = [];
+
+class Tile {
+  constructor(x,y, size) {
+    this.x = x;
+    this.y = y;
+    this.angle = 0;
+    this.size = size;
+    this.type = floor(random(0,2));
+  }
+
+  show() {
+    rectMode(CENTER);
+    push();
+      strokeWeight(.3)
+      translate(this.x,this.y);
+      stroke(0)
+      const offsetX = width / cols / 2;
+      const offsetY = height / rows / 2;
+      rotate(this.angle);
+      noStroke();
+      rect(0,0,this.size,this.size);
+      stroke(3)
+      if(this.type === 0) {
+        arc(-this.size/2,-this.size/2,this.size, this.size, 0, PI/2)
+        arc(this.size/2,this.size/2,this.size, this.size, PI, 3*PI/2)
+      } else {
+        arc(-this.size/2,this.size/2,this.size, this.size, TWO_PI*0.75, TWO_PI)
+        arc(this.size/2,-this.size/2,this.size, this.size, PI/2, PI)
+      }
+    pop();
+  }
+
+  update() {
+    //this.angle += 0.01;
+  }
+
+  detectCursor(x,y) {
+    if(x > this.x - this.size/2 && x < this.x + this.size/2 &&
+       y > this.y - this.size/2 && y < this.y + this.size/2) {
+         this.angle += PI/2;
+       }  
+  }
+
+}
 
 function setup() {
   createCanvas(400, 400);
-  angleMode(DEGREES);
+  // angleMode(DEGREES);
+  const amount = 20; //Amount of tiles
+  const size = width / amount;
+  for(let x = size/2; x < width; x += size) {
+    for(let y = size/2; y < height; y += size) {
+      tiles.push(new Tile(x,y, size));
+    }
+  }
+  //console.log(type)
+  //noLoop();
 }
 
+function draw() {
+  // background(0);
+  tiles.forEach(tile => {
+    tile.update();
+    tile.detectCursor(mouseX, mouseY)
+    tile.show();
+  })
+  
+}
+/*
+// 3. Weird rotating rectangle glitch thing.
+let rows = 20;
+let cols = 20;
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES);
+  //noLoop();
+}
+
+function draw() {
+  background(0);
+  rectMode(CENTER);
+  strokeWeight(.3)
+  //translate(width / 2, height / 2);
+  //noStroke();
+  const offsetX = width / cols / 2;
+  const offsetY = height / rows / 2;
+  for(let x = offsetX; x < width; x += width / cols) {
+    for(let y = offsetY; y < height; y += height / rows) {
+
+  
+      const r = random(0,1)
+      push();
+        //rectMode(CENTER)
+        translate(x,y)
+        rotate(frameCount)
+        scale(map(noise(frameCount) *100, -1, 1, .9, 1.5))
+        //fill(10, map(x, 0, width, 0, 255), map(y, 0, height, 0, 255))
+        rect(0,0,(width/cols),(height/rows));
+
+      pop();
+    }
+  }
+}
+*/
+// 2. Weird gradient with rotating squares
+
+/** 
+let rows = 20;
+let cols = 20;
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES);
+  //noLoop();
+}
+
+function draw() {
+  background(220);
+  //noStroke();
+
+  for(let x = 0; x < width; x += width / cols) {
+    for(let y = 0; y < height; y += height / rows) {
+
+      noStroke();
+      const r = random(0,1)
+      push();
+        rectMode(CENTER)
+        translate(x,y)
+        rotate(frameCount)
+        fill(10, map(x, 0, width, 0, 255), map(y, 0, height, 0, 255))
+        rect(x,y,x+(width/cols),y+(height/rows));
+
+      pop();
+    }
+  }
+}
+*/
+
+/*
+// 1. draws diagonal line using random() function
+let rows = 20;
+let cols = 20;
 function draw() {
   background(220);
   //noStroke();
@@ -14,16 +152,17 @@ function draw() {
 
       //fill(255 * (x / width), 255 * (y / height), 150);
       //rect(x, y, width / cols, height / rows);
-      line(x,y, x + (width/cols), y + (height/rows));
-      line(x,y, x + (width/cols), y - (height/rows));
-      push();
-      if((x+y % 2) === 0) {
-        rotate(20);
-        line(x,y, x, y + (height/rows));
+      const r = random(0,1)
+      if(r < .5) {
+        // line(x,y, x, y + (height/rows));
+        line(x,y, x + (width/cols), y + (height/rows));
+        
+      } else {
+        // line(x,y, x + (width/cols), y);
+        line(x,y, x + (width/cols), y - (height/rows));
+        
       }
-      pop();
-      
-      line(x,y, x + (width/cols), y);
     }
   }
 }
+*/
