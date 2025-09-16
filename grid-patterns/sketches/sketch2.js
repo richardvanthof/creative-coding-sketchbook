fba3b92dd2971ad1f2db1ab2e574823eec1daa84
Richard van 't Hof <hello@therichard.space>
2025-09-16 15:55:15 +0200

    Made triangle pattern generation - made each triangle cell changable - generated each cell based on grid position vs absolute pixel position.

--- grid-patterns/sketches/sketch2.js
+++ grid-patterns/sketches/sketch2.js
@@ -1,33 +1,85 @@
 // triangle grid pattern
+class Triangle {
+    constructor({
+        x, y, // absolute position in pixels
+        s, // Size of cell
+        type, // 0 = pointing up, 1 = pointing down
+        color
+    }) {
+        this.x = x;
+        this.y = y;
+        this.s = s;
+        this.type = type;
+        this.color = 'green';
+    }
+
+    setColor(color) {
+        this.color = color;
+    }
+
+    show(p) {
+        let offset = this.s / 2;
+        let x = this.x * this.s/2 - offset;
+        let y = this.y * this.s;
+        
+        p.fill(this.color);
+        
+        // alternate triangle orientation based on row number
+        // every second row is rotated 180 deg. to make the triangles fit together.
+        p.push();
+            p.translate(x, y);
+            if(this.type === 0) {
+                // upward triangle
+                p.triangle(
+                    offset, 0,
+                    0, this.s,
+                    this.s, this.s
+                )
+            } else {
+                // downward triangle
+                p.triangle(
+                    offset, this.s,
+                    this.s, 0,
+                    0, 0
+                )
+            }
+        p.pop();
+    }
+}
+
 const sketch2 = (p) => {
     let scaleSlider;
+    let trigangleCells = [];
 
     // 6. trigangle grid
-    const resX = 20, resY = resX;
+    const res = 20;
 
     p.setup = function() {
         p.createCanvas(400, 400);
-        p.noLoop();
+        const triangleSize = p.width / res; // Width of each cell
+        for( 
+            let x = 0; 
+            x <= res * 2; // each row has twice the amount of triangles as there are columns;
+            x++
+        ) {
+            trigangleCells[x] = []; // create new column
+            for(let y = 0; y <= res; y++) {
+                trigangleCells[x].push(new Triangle({
+                    x: x,
+                    y: y,
+                    s: triangleSize,
+                    type: x % 2, // alternate type based on position
+                }));
+            };
+        }
+        console.log(trigangleCells);
     };
 
     p.draw = function() {
-        const sx = p.width / resX; // Width of each cell
-        const sy = p.height / resY; // Height of each cell
-        for (let x = 0; x <= p.width; x += sx) {
-            let row = 0;
-            for (let y = 0; y <= p.height; y += sy) {
-                p.fill('green');
-                
-                // alternate triangle orientation based on row number
-                if(row % 2 === 0) {
-                    p.triangle(x, y + sy, x + sx, y + sy, x + sx / 2, y); //upward triangle
-                    p.triangle(x - sx / 2, y, x + sx / 2, y, x, y + sy); //downward triangle
-                } else {
-                    p.triangle(x - sx / 2, y + sy, x + sx / 2, y + sy, x, y); //upward trangle
-                    p.triangle(x, y, x + sx, y, x + sx / 2, y + sy); //downward triangle
-                }
-                row++;
-            }
-        }
+        p.background(255);
+        trigangleCells[1][0].setColor('#ff00ff');
+        trigangleCells.flat().forEach(cell => {
+            cell.show(p); 
+        });
     };
 };
