63bcac15df8ff1a07d6f24f8e2468046386ff51d
Richard van 't Hof <hello@therichard.space>
2025-10-17 23:46:32 +0200

    add pagination id in url + add option for hiding controls

--- interaction/main.js
+++ interaction/main.js
@@ -1,5 +1,6 @@
-import pagination from "./lib/pagination.js";
+import pagination from "../lib/pagination.js";
 import drawPad from "./sketches/drawPad/index.js";
 import reactionDiffusionGenerator from "./sketches/reactionDiffusion/main.js";
+
 
 pagination([reactionDiffusionGenerator, drawPad]);
