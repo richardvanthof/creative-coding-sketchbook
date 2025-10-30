b84de1e66fa96bb1f6ab12274b27a11ca8894573
Richard van 't Hof <hello@therichard.space>
2025-10-30 10:31:00 +0100

    Add boilerplate sketch code

---
+++ extension/main.js
@@ -0,0 +1,6 @@
+import pagination from "../lib/pagination.js";
+
+// import flowfield from "./sketches/flowfield/main.js";
+// import mutualAttraction from "./sketches/mutualAttraction/mutualAttraction.js";
+// import mutualAttractionRevised from "./sketches/mutualAttractionRevised/mutualAttraction.js";
+// pagination([mutualAttraction, mutualAttractionRevised]);
