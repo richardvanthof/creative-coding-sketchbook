45b7bd6de8e8d9b31f207a286d31765da8886ffb
Richard van 't Hof <hello@therichard.space>
2025-11-05 22:29:47 +0100

    Added edge detection back for top of screen.

--- extension/sketches/Ball.js
+++ extension/sketches/Ball.js
@@ -18,9 +18,9 @@ class Ball {
         if (this.pos.x <= 0 || this.pos.x >= this.p.width - this.s) {
             this.vel.x *= -1;
         }
-        // if (this.pos.y <= 0 || this.pos.y >= this.p.height - this.s) {
-        //     this.vel.y *= -1;
-        // }
+        if (this.pos.y <= 0) {
+            this.vel.y *= -1;
+        }
     }
 
     detectBottom() {
