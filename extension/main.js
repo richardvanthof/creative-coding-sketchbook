a0c5adbac5c509b589003558a5ddec277faf8a1a
Richard van 't Hof <hello@therichard.space>
2025-11-05 12:55:34 +0100

    added sketch to main.js

--- extension/main.js
+++ extension/main.js
@@ -1,6 +1,4 @@
 import pagination from "../lib/pagination.js";
+import sketch from './sketches/sketch.js';
 
-// import flowfield from "./sketches/flowfield/main.js";
-// import mutualAttraction from "./sketches/mutualAttraction/mutualAttraction.js";
-// import mutualAttractionRevised from "./sketches/mutualAttractionRevised/mutualAttraction.js";
-// pagination([mutualAttraction, mutualAttractionRevised]);
+pagination([sketch]);
